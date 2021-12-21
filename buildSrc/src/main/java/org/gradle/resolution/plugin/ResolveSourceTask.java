56ac33a81d0b8fb268aa4a08e011d0c51df37e1d
Thomas Tresansky <ttresansky@gradle.com>
2021-12-21 15:02:55 -0500

    Replace copy with file system operations and move artifact view setup outside of task action to fix configuration cache warnings.

--- buildSrc/src/main/java/org/gradle/resolution/plugin/ResolveSourceTask.java
+++ buildSrc/src/main/java/org/gradle/resolution/plugin/ResolveSourceTask.java
@@ -2,10 +2,12 @@ package org.gradle.resolution.plugin;
 
 import org.gradle.api.attributes.Category;
 import org.gradle.api.file.DirectoryProperty;
+import org.gradle.api.file.FileSystemOperations;
 import org.gradle.api.tasks.OutputDirectory;
 import org.gradle.api.tasks.TaskAction;
 import org.gradle.api.artifacts.ArtifactView;
 
+import javax.inject.Inject;
 import java.io.File;
 import java.util.Set;
 import java.util.stream.Collectors;
@@ -14,19 +16,24 @@ public abstract class ResolveSourceTask extends org.gradle.api.DefaultTask {
     @OutputDirectory
     public abstract DirectoryProperty getDestinationDir();
 
+    @Inject
+    protected abstract FileSystemOperations getFileSystemOperations();
+
+
+
+    // Set up an ArtifactView based on the runtime classpath which will fetch documentation
+    private ArtifactView sourcesView = getProject().getConfigurations().getByName("runtimeClasspath").getIncoming().artifactView(view -> {
+        view.setLenient(true);
+        view.getAttributes().attribute(Category.CATEGORY_ATTRIBUTE, getProject().getObjects().named(Category.class, Category.DOCUMENTATION));
+    });
+
     @TaskAction
     public void printSources() {
-        // Set up an ArtifactView based on the runtime classpath which will fetch documentation
-        ArtifactView sourcesView = getProject().getConfigurations().getByName("runtimeClasspath").getIncoming().artifactView(view -> {
-            view.setLenient(true);
-            view.getAttributes().attribute(Category.CATEGORY_ATTRIBUTE, getProject().getObjects().named(Category.class, Category.DOCUMENTATION));
-        });
-
         // Fetch the source files from the ArtifactView
         Set<File> sourceFiles = sourcesView.getFiles().getFiles();
 
         // Copy them to the output folder
-        getProject().copy(spec -> {
+        getFileSystemOperations().copy(spec -> {
             spec.from(sourceFiles);
             spec.into(getDestinationDir());
         });
